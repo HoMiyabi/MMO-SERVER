9da11ed21a012ee8c07f5522193c42d2648b23fc
鼠鼠 <37030246+HoMiyabi@users.noreply.github.com>
2024-08-18 12:40:58 +0800

    Add TcpServer

--- Common/Netwrok/Server/TcpServer.cs
+++ Common/Netwrok/Server/TcpServer.cs
@@ -6,57 +6,67 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GameServer.Network
+namespace Summer.Network
 {
     /// <summary>
     /// 负责监听TCP网络端口，异步接收Socket连接
     /// </summary>
-    public class TcpSocketListener
+    public class TcpServer
     {
         private IPEndPoint endPoint;
         private Socket serverSocket;    //服务端监听对象
+        private int backlog = 100;
 
         public event EventHandler<Socket> SocketConnected; //客户端接入事件
 
-        public TcpSocketListener(string host, int port)
+        public TcpServer(string host, int port)
         {
             endPoint = new IPEndPoint(IPAddress.Parse(host), port);
+        }
+
+        public TcpServer(string host, int port, int backlog): this(host, port)
+        {
+            this.backlog = backlog;
         }
 
         public void Start()
         {
             if (!IsRunning)
             {
-                serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                serverSocket = new Socket(
+                    AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
                 serverSocket.Bind(endPoint);
-                serverSocket.Listen();
+                serverSocket.Listen(backlog);
                 Console.WriteLine("开始监听端口：" + endPoint.Port);
                 
                 SocketAsyncEventArgs args = new();
                 args.Completed += OnAccept; //当有人连入的时候
                 serverSocket.AcceptAsync(args);
-                
+            }
+            else
+            {
+                Console.WriteLine("TcpServer is already running.");
             }
         }
 
         private void OnAccept(object sender, SocketAsyncEventArgs e)
         {
-            //真的有人连进来
+            Socket client = e.AcceptSocket; //连入的人
+
+            //继续接收下一位
+            e.AcceptSocket = null;
+            serverSocket.AcceptAsync(e);
+
+            // 真的有人连进来
             if (e.SocketError == SocketError.Success)
             {
-                Socket client = e.AcceptSocket; //连入的人
                 if (client != null)
                 {
                     SocketConnected?.Invoke(this, client);
                 }
             }
-            
-
-            //继续接收下一位
-            e.AcceptSocket = null;
-            serverSocket.AcceptAsync(e);
         }
-         
+
         public bool IsRunning
         {
             get { return serverSocket != null; }
