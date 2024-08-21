9396b76ec0aa75c2a7da5b070889f1426eef73a0
鼠鼠 <37030246+HoMiyabi@users.noreply.github.com>
2024-08-21 15:29:40 +0800

    Update protoc script for auto copy

--- protos/protoc.ps1
+++ protos/protoc.ps1
@@ -2,6 +2,9 @@ $inputPath = "./protos"
 $outputPath = "./csharp"
 $protoFilter = "*.proto"
 
+$projectPath1 = "C:\Things\Code\CSharp\MMO-SERVER\Common\Proto"
+$projectPath2 = "C:\UnityProjects\MMOGAME\Assets\Plugins\Summer\Proto"
+
 if (!(Test-Path $outputPath -PathType Container)) {
     New-Item -ItemType Directory -Force -Path $outputPath
 }
@@ -18,5 +21,13 @@ foreach ($file in $files)
         "--csharp_out", $outputPath,
         $filename)
 }
+
+# Copy to project
+
+Get-ChildItem -Path $projectPath1 -Recurse | Remove-Item
+Get-ChildItem -Path $outputPath -Recurse | Copy-Item -Destination $projectPath1
+
+Get-ChildItem -Path $projectPath2 -Recurse | Remove-Item
+Get-ChildItem -Path $outputPath -Recurse | Copy-Item -Destination $projectPath2
 
 Pause
